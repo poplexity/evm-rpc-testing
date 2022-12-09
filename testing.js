4d0d0f36c866fb75c9097c814a34dd80793366e8
Jesse Schulman <jesse@caleos.io>
2022-12-09 15:30:13 -0800

    Better format sync status

--- testing.js
+++ testing.js
@@ -3,7 +3,8 @@ const { ethers } = require("ethers");
 const test = async() => {
     const provider = new ethers.providers.JsonRpcProvider("http://localhost:8545");
     const blockNumber = await provider.getBlockNumber();
-    console.log(`Syncing status: ${await provider.send("eth_syncing", [])}`)
+    console.log(`Syncing status:`)
+    console.dir(await provider.send("eth_syncing", []))
     console.log(`Block number is: ${blockNumber}`)
 }
 
