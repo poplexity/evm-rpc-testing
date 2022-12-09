62db9aa959b7263b98f2ae7726ce1b3653a690ca
Jesse Schulman <jesse@caleos.io>
2022-12-09 15:29:24 -0800

    Adding sync status

--- testing.js
+++ testing.js
@@ -3,6 +3,7 @@ const { ethers } = require("ethers");
 const test = async() => {
     const provider = new ethers.providers.JsonRpcProvider("http://localhost:8545");
     const blockNumber = await provider.getBlockNumber();
+    console.log(`Syncing status: ${await provider.send("eth_syncing", [])}`)
     console.log(`Block number is: ${blockNumber}`)
 }
 
